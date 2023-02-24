837f5ac6e5ebfbea7ddccb9d3e3ba66748c6b91d
StochajK <stochaj.k@northeastern.edu>
2023-02-24 11:17:44 -0500

    fix sorting method

--- HeartDiseaseOneHot.py
+++ HeartDiseaseOneHot.py
@@ -12,6 +12,8 @@ import pandas as pd
 DESIRED_COLS = ["age", "sex", "chest pain type", "resting bp s", "cholesterol", "fasting blood sugar", "target"]
 NUMERIC_COLS = ["age", "chest pain type", "resting bp s", "cholesterol", "max heart rate", "oldpeak"]
 NUM_BUCKETS = 4
+TREATMENT = "cholesterol"
+HIGH_LOW = 1
 
 def main():
     # read in the data file
@@ -23,10 +25,18 @@ def main():
     
     # "bucketize" necessary columns, perform one hot encoding
     for col in NUMERIC_COLS:
-        if col in df_heart.columns:
+        if col in df_heart.columns and col != TREATMENT:
             new_cols = pd.get_dummies(pd.qcut(df_heart[col], q=NUM_BUCKETS, duplicates = "drop"), prefix = col)
             df_heart[new_cols.columns] = new_cols
             df_heart.drop(col, axis = 1, inplace = True)
+        elif col == TREATMENT:
+            new_cols = pd.get_dummies(pd.qcut(df_heart[col], q=2, duplicates = "drop"), prefix = col)
+            df_heart[new_cols.columns] = new_cols
+            df_heart.drop(col, axis = 1, inplace = True)
+            new_treatment = new_cols.columns.to_list()[HIGH_LOW]
+    
+    if TREATMENT not in NUMERIC_COLS:
+        new_treatment = TREATMENT
             
     # move the target column to the last position (mostly aesthetics)
     all_cols = df_heart.columns.to_list()
@@ -35,9 +45,15 @@ def main():
     df_heart = df_heart[all_cols]
     
     # split the data frame based on the target value
-    grouped_df = df_heart.groupby("target")
+    grouped_df = df_heart.groupby(new_treatment)
+    
     df_no_disease = grouped_df.get_group(0)
+    df_no_disease.reset_index(inplace = True)
+    df_no_disease.drop("index", axis = 1, inplace = True)
+    
     df_yes_disease = grouped_df.get_group(1)
+    df_yes_disease.reset_index(inplace = True)
+    df_yes_disease.drop("index", axis = 1, inplace = True)
                
     # write new df to a csv file
     df_heart.to_csv("Binary_HeartData.csv")
